8ed291b8099474e430b70b2fd634b2310e496b20
muyawia <muyawia_kashif@outlook.com>
2022-08-07 20:41:16 +0100

    add js code for timer

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 3-week-coursework
+# Timed Quiz
+
+## Description
+A timed quiz using vanilla javascript that punishes mistakes and times the participant creating a score.
+
+## Usage
+This application is not deployed live on any website.
+
+## TODO:
+* Add questions
+* Add styling
+* Flesh HTML out
+* Time the particpant's answer time to a series of questions
+* Allow reattempts and save a highscore value
+* Time score penalities for falling to answer correctly
+* Start timer button and logic
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-var timer = document.getElementById('count');
+//make the html variable p in to a variable using its id
+var Eltimer = document.getElementById('count');
+//set variables and values for the start time and a time variable to be operated on mathmatically within the javascript 
+var startTime = 10;
+var time = startTime * 60;
+//the function that executes mathmatics on time and displays the result
+function timer () {
+    //divide time to find the minutes
+    var minute = Math.floor(time / 60);
+    //divide time further to find the seconds using the remainder operator so there is no weird rounding errors
+    var second = time % 60;
+    second = second < 10 ? '0' + second : second;
+    Eltimer.innerHTML = `${minute} : ${second}`;
+    //decrease the time at the end of the function
+    time--;
+}
+//calls the function every 'second'
+setInterval(timer, 1000);
